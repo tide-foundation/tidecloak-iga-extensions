d23ca26ec42dbed905cc3c613dc3fc1c578b6d16
Sasha Le <iamsasha.le@gmail.com>
2024-10-31 12:35:55 +1000

    testing iga toggle

---
+++ tidecloak-resource-providers/src/main/java/org/tidecloak/TideResource/TideAdminRealmResource.java
@@ -0,0 +1,62 @@
+package org.tidecloak.TideResource;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.jboss.logging.Logger;
+import org.jboss.resteasy.reactive.server.multipart.FormValue;
+import org.jboss.resteasy.reactive.server.multipart.MultipartFormDataInput;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.services.resources.admin.permissions.AdminPermissionEvaluator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Map;
+
+
+public class TideAdminRealmResource {
+
+    private final KeycloakSession session;
+    private final RealmModel realm;
+    private final AdminPermissionEvaluator auth;
+
+    protected static final Logger logger = Logger.getLogger(TideAdminRealmResource.class);
+    public TideAdminRealmResource(KeycloakSession session, RealmModel realm, AdminPermissionEvaluator auth) {
+        this.session = session;
+        this.realm = realm;
+        this.auth = auth;
+    }
+    @POST
+    @Path("toggle-iga")
+    @Produces(MediaType.TEXT_PLAIN)
+
+    public Response toggleIGA(@FormParam("isIGAEnabled") boolean isEnabled) {
+        try{
+            auth.realm().requireManageRealm();
+            session.getContext().getRealm().setAttribute("isIGAEnabled", isEnabled);
+
+            logger.info("IGA has been toggled to : " + isEnabled);
+            return buildResponse(200, "IGA has been toggled to : " + isEnabled);
+        }catch(Exception e) {
+            logger.error("Error toggling IGA on realm: ", e);
+            throw e;
+        }
+
+    }
+
+    private Response buildResponse(int status, String message) {
+        return Response.status(status)
+                .header("Access-Control-Allow-Origin", "*")
+                .entity(message)
+                .type(MediaType.TEXT_PLAIN)
+                .build();
+    }
+
+
+}
+
+
---
+++ tidecloak-resource-providers/src/main/java/org/tidecloak/TideResource/TideAdminRealmResourceProvider.java
@@ -0,0 +1,44 @@
+package org.tidecloak.TideResource;
+
+import org.keycloak.Config;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.models.RealmModel;
+import org.keycloak.services.resources.admin.AdminEventBuilder;
+import org.keycloak.services.resources.admin.ext.AdminRealmResourceProvider;
+import org.keycloak.services.resources.admin.ext.AdminRealmResourceProviderFactory;
+import org.keycloak.services.resources.admin.permissions.AdminPermissionEvaluator;
+
+public class TideAdminRealmResourceProvider
+        implements AdminRealmResourceProviderFactory, AdminRealmResourceProvider {
+
+    public static final String PROVIDER_ID = "tideAdminResources";
+
+    @Override
+    public AdminRealmResourceProvider create(KeycloakSession session) {
+        return this;
+    }
+
+    @Override
+    public Object getResource(KeycloakSession session, RealmModel realm, AdminPermissionEvaluator auth, AdminEventBuilder adminEvent) {
+        return new TideAdminRealmResource(session, realm, auth);
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+}
