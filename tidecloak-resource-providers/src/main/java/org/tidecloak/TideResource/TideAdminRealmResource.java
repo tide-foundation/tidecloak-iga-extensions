03c09b333301577399189cb1000f9b57f86429ff
Sasha Le <iamsasha.le@gmail.com>
2024-11-05 15:43:21 +1000

    fix

--- tidecloak-resource-providers/src/main/java/org/tidecloak/TideResource/TideAdminRealmResource.java
+++ tidecloak-resource-providers/src/main/java/org/tidecloak/TideResource/TideAdminRealmResource.java
@@ -14,6 +14,7 @@ import org.keycloak.models.RealmModel;
 import org.keycloak.services.resources.admin.permissions.AdminPermissionEvaluator;
 import org.midgard.Midgard;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TideAdminRealmResource {
