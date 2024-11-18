ac346a5408cd7982e50c58191c3da93eaad7a70e
Sasha Le <iamsasha.le@gmail.com>
2024-11-18 15:58:35 +1000

    add missing package

--- tide-jpa-providers/src/main/java/org/tidecloak/jpa/utils/IGAUtils.java
+++ tide-jpa-providers/src/main/java/org/tidecloak/jpa/utils/IGAUtils.java
@@ -2,6 +2,9 @@ package org.tidecloak.jpa.utils;
 
 import org.keycloak.models.RealmModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IGAUtils {
     public static boolean isIGAEnabled(RealmModel realm) {
         String isIGAEnabled = realm.getAttribute("isIGAEnabled");
