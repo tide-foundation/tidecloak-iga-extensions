46546d321ce2be2eeb67a1960cab118fb5394015
Sasha Le <iamsasha.le@gmail.com>
2025-06-27 20:17:48 +1000

    add constants

---
+++ shared-models/src/main/java/org/tidecloak/shared/Constants.java
@@ -0,0 +1,14 @@
+package org.tidecloak.shared;
+
+
+public class Constants {
+
+    public static final String TIDE_REALM_ADMIN = "tide-realm-admin";
+    public static final String TIDE_VENDOR_KEY = "tide-vendor-key";
+    public static final String TIDE_IDP_ALIAS = "tide";
+    public static final String TIDE_POINT_G = "";
+    public static final String TIDE_ORDER = "";
+    public static final String TIDE_INITIAL_AUTHORIZER = "firstAdmin";
+    public static final String TIDE_MULTI_ADMIN_AUTHORIZER = "multiAdmin";
+
+}
