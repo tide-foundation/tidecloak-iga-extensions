9e3e128e175e3cbbfd9ca9b23e75d9d6c1494a1d
sashyo <iamsasha.le@gmail.com>
2025-06-30 11:31:29 +1000

    Update Constants.java

--- shared-models/src/main/java/org/tidecloak/shared/Constants.java
+++ shared-models/src/main/java/org/tidecloak/shared/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
     public static final String TIDE_REALM_ADMIN = "tide-realm-admin";
     public static final String TIDE_VENDOR_KEY = "tide-vendor-key";
     public static final String TIDE_IDP_ALIAS = "tide";
-    public static final String TIDE_POINT_G = "";
-    public static final String TIDE_ORDER = "";
+    public static final String TIDE_POINT_G = "WGZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmY=";
+    public static final String TIDE_ORDER = "7237005577332262213973186563042994240857116359379907606001950938285454250989";
     public static final String TIDE_INITIAL_AUTHORIZER = "firstAdmin";
     public static final String TIDE_MULTI_ADMIN_AUTHORIZER = "multiAdmin";
 
