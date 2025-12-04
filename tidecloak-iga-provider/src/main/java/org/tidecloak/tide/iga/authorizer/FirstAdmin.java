1ab198bb9806c0a0c8ce652f44ecbde21c4d465c
Sasha Le <iamsasha.le@gmail.com>
2025-12-04 16:26:07 +1000

    fix client approvals

--- tidecloak-iga-provider/src/main/java/org/tidecloak/tide/iga/authorizer/FirstAdmin.java
+++ tidecloak-iga-provider/src/main/java/org/tidecloak/tide/iga/authorizer/FirstAdmin.java
@@ -9,6 +9,7 @@ import org.keycloak.models.Constants;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.RoleModel;
+import org.keycloak.models.jpa.entities.ClientEntity;
 import org.keycloak.models.jpa.entities.RoleEntity;
 import org.keycloak.services.resources.admin.AdminAuth;
 import org.midgard.models.UserContext.UserContext;
@@ -19,8 +20,11 @@ import org.tidecloak.base.iga.utils.BasicIGAUtils;
 import org.tidecloak.jpa.entities.AccessProofDetailEntity;
 import org.tidecloak.jpa.entities.AuthorizerEntity;
 import org.tidecloak.jpa.entities.ChangesetRequestEntity;
+import org.tidecloak.jpa.entities.drafting.TideClientDraftEntity;
 import org.tidecloak.jpa.entities.drafting.TideRoleDraftEntity;
 import org.tidecloak.jpa.entities.drafting.TideUserRoleMappingDraftEntity;
+import org.tidecloak.shared.enums.ActionType;
+import org.tidecloak.shared.enums.DraftStatus;
 import org.tidecloak.shared.enums.ChangeSetType;
 import org.tidecloak.shared.enums.WorkflowType;
 import org.tidecloak.shared.enums.models.WorkflowParams;
@@ -110,10 +114,51 @@ public class FirstAdmin implements Authorizer {
             RoleModel role = realm.getRoleById(tideUserRoleMappingDraftEntity.getRoleId());
             if (role.getName().equalsIgnoreCase(org.tidecloak.shared.Constants.TIDE_REALM_ADMIN)){
                 authorizer.setType("multiAdmin");
+                // Regenerate default user contexts for all clients when transitioning to multiAdmin
+                regenerateDefaultUserContexts(session, realm, em, processorFactory);
             }
         }
         em.flush();
         return Response.ok("Change set approved and committed with authorizer type:  " + authorizer.getType()).build();
+    }
+
+    /**
+     * Regenerates default user contexts for all clients in the realm.
+     * Called when transitioning from firstAdmin to multiAdmin mode.
+     */
+    private void regenerateDefaultUserContexts(KeycloakSession session, RealmModel realm, EntityManager em, ChangeSetProcessorFactory processorFactory) throws Exception {
+        List<ClientModel> clients = realm.getClientsStream().toList();
+
+        for (ClientModel clientModel : clients) {
+            ClientEntity client = em.find(ClientEntity.class, clientModel.getId());
+            if (client == null) continue;
+
+            List<TideClientDraftEntity> clientDrafts = em.createNamedQuery("getClientFullScopeStatus", TideClientDraftEntity.class)
+                    .setParameter("client", client)
+                    .getResultList();
+            if (clientDrafts.isEmpty()) continue;
+
+            for (TideClientDraftEntity draft : clientDrafts) {
+                // Remove existing Access Proofs
+                List<AccessProofDetailEntity> accessProof = em.createNamedQuery("getProofDetailsForDraft", AccessProofDetailEntity.class)
+                        .setParameter("recordId", draft.getChangeRequestId()).getResultList();
+                accessProof.forEach(em::remove);
+
+                // Remove existing ChangeSetRequestEntity
+                ChangesetRequestEntity changesetRequestEntity = em.find(ChangesetRequestEntity.class,
+                        new ChangesetRequestEntity.Key(draft.getChangeRequestId(), ChangeSetType.CLIENT));
+                if (changesetRequestEntity != null) {
+                    changesetRequestEntity.getAdminAuthorizations().clear();
+                    em.remove(changesetRequestEntity);
+                }
+
+                // Execute Workflow to regenerate
+                draft.setDraftStatus(DraftStatus.DRAFT);
+                WorkflowParams params = new WorkflowParams(DraftStatus.DRAFT, false, ActionType.CREATE, ChangeSetType.CLIENT);
+                processorFactory.getProcessor(ChangeSetType.CLIENT)
+                        .executeWorkflow(session, draft, em, WorkflowType.REQUEST, params, null);
+            }
+        }
     }
 
     private boolean isAssigningTideRealmAdminRole(Object draftEntity, KeycloakSession session){
